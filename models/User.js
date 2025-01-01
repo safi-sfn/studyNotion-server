285e7be6912ee552256ff6435f3982b6348e1ab4
Safi-sfn <mdsafiruddin24@gmail.com>
2025-01-01 15:17:20 +0530

    removed 1 line

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
-        required:true,
         trim:true
     },
     lastName:{
